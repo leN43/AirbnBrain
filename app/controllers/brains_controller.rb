615209972edb00ce1751e2eec05c39fdc1d3e6c3
Audrey Pachol <audrey.pachol@gmail.com>
2023-06-19 17:36:02 +0200

    correction braincontroller

--- app/controllers/brains_controller.rb
+++ app/controllers/brains_controller.rb
@@ -1,35 +1,4 @@
 class BrainsController < ApplicationController
-  before_action :set_brain, %i[edit show create destroy]
-
-  def new
-    @brain = Brain.new
-  end
-
-  def index
-    @brains = Brain.all
-  end
-
-  def show; end
-
-  def edit
-  end
-
-  def destroy
-    @brain.destroy
-    redirect_to brains_path, status: :see_other
-  end
-
-  private
-
-  def brain_params
-    params.require(:brain).permit(:name, :description, :price)
-  end
-
-  def set_brain
-    @brain = Brain.find(params[:id])
-  end
-end
-class BrainsController < ApplicationController
   before_action :set_brain, %i[edit show create update destroy]
 
   def new
