90e24d40d52145426b46ae960166cbe0eedd5a03
Audrey Pachol <audrey.pachol@gmail.com>
2023-06-19 16:33:33 +0200

    braincontroller new index private and devisecontroller add last and first names and views for home and new and index

--- app/controllers/brains_controller.rb
+++ app/controllers/brains_controller.rb
@@ -1,10 +1,31 @@
 class BrainsController < ApplicationController
-  def :new
+  before_action :set_brain, %i[edit show create destroy]
+
+
+
+  def new
+    @brain = Brain.new
   end
 
-  def :index
+  def index
+    @brains = Brain.all
   end
 
-  def :edit
+  def show; end
+
+  def edit
+  end
+
+  def destroy
+  end
+
+  private
+
+  def brain_params
+    params.require(:brain).permit(:name, :description, :price)
+  end
+
+  def set_brain
+    @brain = Brain.find(params[:id])
   end
 end
