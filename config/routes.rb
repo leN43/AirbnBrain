20ab2bfa3a43633e2e1854b2db64fd94131835dc
Niels Ayigbede <niels.ayigbede@gmail.com>
2023-06-20 11:20:43 +0200

    test

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-<<<<<<< HEAD
-<<<<<<< HEAD
   root to: "pages#home"
-=======
-  root to: "home#index"
->>>>>>> a2329e364873dcbe31b643f7302c07d1f38fdd11
-=======
-  root to: "pages#home"
->>>>>>> dff129367948e579137468bea0b29cca9921c8b5
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :brains do
     resources :bookings, only: [:create]
