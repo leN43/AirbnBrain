3e87ee10e0d80e2086263870c0f7aff864a7a380
Audrey Pachol <audrey.pachol@gmail.com>
2023-06-20 12:27:49 +0200

    update create braincontroller
    Signed-off-by: Audrey Pachol <audrey.pachol@gmail.com>

--- app/controllers/brains_controller.rb
+++ app/controllers/brains_controller.rb
@@ -1,11 +1,13 @@
 class BrainsController < ApplicationController
   before_action :set_brain, except: %i[new create index]
+  before_action :set_user
 
   def new
     @brain = Brain.new
   end
 
   def create
+    params[:brain][:user_id] = current_user.id
     @brain = Brain.new(brain_params)
     if @brain.save
       redirect_to brain_path(@brain)
@@ -20,26 +22,37 @@ class BrainsController < ApplicationController
 
   def show; end
 
-  def edit
-  end
+  def edit; end
 
   def update
-    @brain.update(brain_params)
-    redirect_to brain_path(@brain)
+    if current_user.id == @brain.user_id
+      @brain.update(brain_params)
+      redirect_to brain_path(@brain)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def destroy
-    @brain.destroy
-    redirect_to brains_path, status: :see_other
+    if current_user.id == @brain.user_id
+      @brain.destroy
+      redirect_to brains_path, status: :see_other
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   private
 
   def brain_params
-    params.require(:brain).permit(:id, :name, :description, :price)
+    params.require(:brain).permit(:name, :description, :price, :user_id)
   end
 
   def set_brain
     @brain = Brain.find(params[:id])
+  end
+
+  def set_user
+    @user = current_user
   end
 end
