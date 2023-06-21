b3c7ef8bb72a6896a695b58dac87fab3eba4f784
Leslie Rados <leslie.rados@gmail.com>
2023-06-21 12:11:21 +0200

    ajout catégorie et image à Brain

--- app/models/brain.rb
+++ app/models/brain.rb
@@ -1,5 +1,8 @@
 class Brain < ApplicationRecord
+  CATEGORIES = ['Négociatrice', 'Perfectionniste', 'Drama Queen', 'Macchiavélique', 'Contrepéteur', 'Urgentiste', 'Chanteur de douche', 'Poesie de la lune']
+
   has_many :bookings, dependent: :destroy
   has_many :users, through: :bookings, dependent: :destroy
   validates :name, :description, :price, presence: true, uniqueness: true
+  validates :categories, inclusion: { in: CATEGORIES }
 end
